f2dbc2c693c224f7f093ded985738c1dacec0d3d
vijayareddy <reddyvijaya1994@gmail.com>
2019-06-03 17:22:41 +0530

    calculations added:

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -15,6 +15,7 @@
         <td>{{ props.item.product.name }}</td>
         <td>{{ }}</td>
         <td>{{ }}</td>
+
         <td>
           <v-edit-dialog :return-value.sync="props.item.quantity">
             {{props.item.quantity}}
@@ -27,6 +28,7 @@
           </v-edit-dialog>
         </td>
         <td>{{ props.item.product.mrp }}</td>
+        <td>{{props.item.product.gst}}</td>
         <td>
           <v-edit-dialog :return-value.sync="props.item.discount">
             {{props.item.discount}}
@@ -35,13 +37,17 @@
             </template>
           </v-edit-dialog>
         </td>
-        <td>{{ props.item.amount }}</td>
+        <td>{{( props.item.product.mrp * props.item.quantity * props.item.discount)/100}}</td>
+        <td>{{ props.item.amount}}</td>
         <td>
           <v-icon small @click="deleteItem(props.item)">delete</v-icon>
         </td>
       </template>
     </v-data-table>
     <v-toolbar flat color="white">
+    <v-toolbar-title>Gross Amount:{{pos.GrossAmount}}</v-toolbar-title>
+    <v-toolbar-title>Discount:{{pos.totalDiscount}}</v-toolbar-title>
+    <v-toolbar-title>GST:{{pos.gst}}</v-toolbar-title>
       <v-toolbar-title>Total Amount : {{ pos.totalAmount }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn color="secondary" class="mb-1" flat @click="clearPos()">Clear Cart</v-btn>
@@ -70,20 +76,26 @@ export default {
       { text: "Exp Date", value: "expDate" },
       { text: "Quantity", value: "quantity" },
       { text: "MRP", value: "mrp" },
-      { text: "discount %", value: "discount" },
+      {text: "GST%", value:"gst"},
+      { text: "disc%", value: "discount" },
+      {text: "discAmount", value:"discountAmount"},
       { text: "amount", value: "amount" },
       { text: "Actions", value: "name", sortable: false }
     ],
     pos: {
       customer: {},
       lineItems: [],
-      totalAmount: 0
+      totalAmount: 0,
+      GrossAmount:0,
+      gst:0,
+      totalDiscount:0
     },
     lineItem: {
       product: {},
       quantity: 1,
       discount: 0,
-      amount: 0
+      amount: 0,
+      gst:0
     },
     editedIndex: -1
   }),
@@ -153,16 +165,47 @@ export default {
         .reduce((prev, next) => prev + next);
     },
 
+    gstTotal(){
+    this.pos.gst = this.pos.lineItems
+      .map(e => e.gst)
+      .reduce((prev, next) =>prev + next);
+    },
+
+    grossAmount(){
+    this.pos.GrossAmount = this.pos.lineItems
+      .map(e => e.amount)
+      .reduce((prev,next) => prev + next);
+    },
+
+    totalDiscount(){
+    this.pos.totalDiscount = this.pos.lineItems
+        .map(e => e.discount)
+        .reduce((prev,next) => prev + next);
+    },
+
     calculateAmount(item, value) {
       const index = this.pos.lineItems.indexOf(item);
 
       let amount = value * this.pos.lineItems[index].product.mrp;
 
+      let gst = amount * this.pos.lineItems[index].product.gst/100;
+
+      console.log(gst);
+
       let discountAmount = (amount * this.pos.lineItems[index].discount) / 100;
 
-      this.pos.lineItems[index].amount = amount - discountAmount;
+      this.pos.lineItems[index].amount = (amount + gst) - discountAmount;
+
+      this.pos.lineItems[index].amount = amount;
+
+      this.pos.lineItems[index].gst = gst;
+
+      this.pos.lineItems[index].discountAmount = discountAmount;
 
       this.netTotal();
+      this.gstTotal();
+      this.grossAmount();
+      this.totalDiscount();
     },
 
     proceed(data) {
